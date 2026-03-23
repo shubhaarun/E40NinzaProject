38a199e84646e3ea493a463426050e13c9ae4cd3
Arun Kiran <Arun Kiran@DESKTOP-G6Q509C>
2026-03-23 17:22:10 +0530

    commit

---
+++ DummyE40/src/test/java/Demo/Program1.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class Program1 {
+public static void main(String[] args) {
+	System.out.println("hellooo");
+	System.out.println("hellooo");
+	System.out.println("hellooo");
+}
+}
